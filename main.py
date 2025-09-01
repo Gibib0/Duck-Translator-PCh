4631444ea91b43a5792585b901d2ab04ab687308
Anastasia <anast111antonova@gmail.com>
2025-09-01 10:04:35 +0300

    Add libraries

---
+++ main.py
@@ -0,0 +1,14 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.popup import Popup
+from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.core.audio import SoundLoader
+from kivy.graphics import Color, Rectangle
+import random
+import os
+
